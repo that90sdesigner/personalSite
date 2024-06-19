369768727aa58e20970e5926aba9b641b914ec1e
that90sdesigner <121989242+that90sdesigner@users.noreply.github.com>
2024-06-19 11:08:17 -0700

    Added vite config to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,3 +4,12 @@ export default defineConfig({});
 {
     site: 'https://www.shankarux.com'
   }
+
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      external: ['@astrojs/rss']
+    }
+  }
+}
