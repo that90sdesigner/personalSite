3addbe2c9d7a73b855030f209291e237a7d681e6
Shankar <121989242+that90sdesigner@users.noreply.github.com>
2025-04-06 16:14:56 -0700

    Fixed syntax errors

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 // vite.config.js
 export default {
-    build: {
-      rollupOptions: {
-        external: ['@astrojs/rss']
-        build.rollupOptions.external: ['markdown-it', 'sanitize-html']
-      }
+  build: {
+    rollupOptions: {
+      external: ['@astrojs/rss', 'markdown-it', 'sanitize-html']
     }
   }
+};
+
+//OLD CODE
+// export default {
+//     build: {
+//       rollupOptions: {
+//         external: ['@astrojs/rss']
+//         build.rollupOptions.external: ['markdown-it', 'sanitize-html']
+//       }
+//     }
+//   }
