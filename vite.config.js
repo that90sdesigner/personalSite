f592dcd9750522d05d3b71da2b1fd0aa91c6861c
Shankar <121989242+that90sdesigner@users.noreply.github.com>
2025-04-06 16:04:41 -0700

    Updated vite config
    rolling up sanitize-html and markdown-it

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ export default {
     build: {
       rollupOptions: {
         external: ['@astrojs/rss']
+        build.rollupOptions.external: ['markdown-it', 'sanitize-html']
       }
     }
   }
